1966c29b87789e93d368a2631ff1b509c01d8049
Julien PETOT <julien.petot@yahoo.fr>
2021-01-21 10:55:01 +0100

    explo

--- script/0_exploratory_analysis/heatmap_dynamic.R
+++ script/0_exploratory_analysis/heatmap_dynamic.R
@@ -1,31 +1,39 @@
 
-data = read.table
+data = read.table("data/gazedata/18-damien/Result2/ScreenRecorderPath.dat", skip = 1, header = FALSE)
 
-heatmap = ggplot(data, aes(x = new_x, y = new_y) ) +
+data = data[,1:3]
+
+data = gaze_preprocess(data, c(9,16))
+
+data = remove_first_time(data,15)
+data = remove_last_time(data,27.51)
+
+
+path_img ="experience/cockpit_utile/112.png"
+img <- readPNG(path_img)
+img <- rasterGrob(img, interpolate=TRUE)
+
+ggplot(data, aes(x = x, y = y) ) +
   
   theme_void() +
   annotation_custom(img, xmin=0, xmax=16, ymin=0, ymax=9) +
   
-  stat_density_2d(aes(fill = ..density..), 
+  stat_density_2d_filled(aes(fill = ..density..), 
                   geom = "raster", 
-                  contour = FALSE, 
-                  alpha = transparency_img) +
+                  contour = FALSE,
+                  contour_var = "count",
+                  alpha = 0.8) +
   
   scale_fill_distiller(palette= "Spectral", direction=-1) +
-  scale_x_continuous(limits = c(0, 16)) +
-  scale_y_continuous(limits = c(0, 9)) +
+  scale_x_continuous(limits = c(-10, 18)) +
+  scale_y_continuous(limits = c(-3, 12)) +
   
-  coord_fixed(ratio = 1, xlim = c(0,16), ylim = c(0,9)) +
+  coord_fixed(ratio = 1, xlim = c(-10, 18), ylim = c(-3, 12)) +
   
   theme(
     legend.position='none', 
     axis.title.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.title.y = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank()
-  )
+    axis.title.y = element_blank()  )
 }
 
 
