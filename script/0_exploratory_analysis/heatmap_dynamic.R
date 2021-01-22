9cdab61ebb0ad43952dc23e8430432669febb8e9
Julien PETOT <julien.petot@yahoo.fr>
2021-01-22 12:25:31 +0100

    exploratory visu for dynamic heatmap

--- script/0_exploratory_analysis/heatmap_dynamic.R
+++ script/0_exploratory_analysis/heatmap_dynamic.R
@@ -1,4 +1,14 @@
 
+library(eyetrackingR)
+
+library(parameters)
+library(NbClust)
+
+library(plyr)
+
+source("script/requirements.R")
+source("script/1_data_processing/helpers_data_process.R")
+
 data = read.table("data/gazedata/18-damien/Result2/ScreenRecorderPath.dat", skip = 1, header = FALSE)
 
 data = data[,1:3]
@@ -13,86 +23,48 @@ path_img ="experience/cockpit_utile/112.png"
 img <- readPNG(path_img)
 img <- rasterGrob(img, interpolate=TRUE)
 
-ggplot(data, aes(x = x, y = y) ) +
-  
-  theme_void() +
+data.pca = PCA(data, col.w = c(1,1,1))
+
+data.hcpc = HCPC(res = data.pca, nb.clust = -1)
+
+
+
+find_hull <- function(df) df[chull(df$x, df$y), ]
+poly <- ddply(data.hcpc$data.clust, "clust", find_hull)
+
+plot <- ggplot(data = data.hcpc$data.clust, aes(x = x,  y = y, colour=clust, fill = clust)) +
   annotation_custom(img, xmin=0, xmax=16, ymin=0, ymax=9) +
+  geom_point() + 
+  geom_polygon(data = poly, alpha = 0.5) +
+  labs(x = "x", y = "y")+
   
-  stat_density_2d_filled(aes(fill = ..density..), 
+  coord_fixed(ratio = 1, xlim = c(-10, 18), ylim = c(-3, 12)) 
+plot
+
+cluster = data.hcpc$data.clust
+
+
+ggplot()+
+  annotation_custom(img, xmin=0, xmax=16, ymin=0, ymax=9) +
+  geom_point(data = cluster, aes(x = x,  y = y, colour = clust)) + 
+  geom_polygon(data = poly, aes(x = x,  y = y, colour=clust, fill = clust), alpha = 0.5) +
+  coord_fixed(ratio = 1, xlim = c(-10, 18), ylim = c(-3, 12)) 
+
+
+ggplot() +
+    annotation_custom(img, xmin=0, xmax=16, ymin=0, ymax=9) +
+  
+  stat_density_2d_filled(data = cluster , aes(x = x,  y = y, fill = ..density..), 
                   geom = "raster", 
                   contour = FALSE,
                   contour_var = "count",
                   alpha = 0.8) +
-  
+  geom_point(data = data.hcpc$data.clust, aes(x = x,  y = y, colour = t, shape = clust)) +
   scale_fill_distiller(palette= "Spectral", direction=-1) +
   scale_x_continuous(limits = c(-10, 18)) +
-  scale_y_continuous(limits = c(-3, 12)) +
+  scale_y_continuous(limits = c(-3, 12)) 
   
-  coord_fixed(ratio = 1, xlim = c(-10, 18), ylim = c(-3, 12)) +
-  
-  theme(
-    legend.position='none', 
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank()  )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 
